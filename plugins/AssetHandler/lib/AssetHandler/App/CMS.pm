b659f436e10cb65c5550a3edc67243b1c3dbd8ca
naoaki011 <naoaki@orion.ocn.ne.jp>
2011-07-03 22:51:51 +0900

    Merge AssetUnlink.

--- plugins/AssetHandler/lib/AssetHandler/App/CMS.pm
+++ plugins/AssetHandler/lib/AssetHandler/App/CMS.pm
@@ -6,6 +6,7 @@ use MT::Util qw( format_ts relative_date caturl );
 use MT 4.0;
 use MT::Asset;
 use File::Spec;
+use base qw( MT::Object );
 
 sub open_batch_editor {
     my ($app) = @_;
@@ -456,6 +457,34 @@ sub no_assoc_terms {
     return;
 }
 
+sub unlink_asset {
+    my ($app) = @_;
+    my @ids = $app->param('id');
+    require MT::Asset;
+    foreach my $id (@ids) {
+        my $asset = MT::Asset->load($id);
+#        $asset->remove_cached_files;
+        # remove children.
+#        my $class = ref $asset;
+#        my $iter = __PACKAGE__->load_iter({ parent => $asset->id, class => '*' });
+#        while(my $a = $iter->()) {
+#            $a->SUPER::remove;
+#        }
+        # Remove MT::ObjectAsset records
+        my $class = MT->model('objectasset');
+        my $iter = $class->load_iter({ asset_id => $asset->id });
+        while (my $o = $iter->()) {
+            $o->remove;
+        }
+        $asset->SUPER::remove;
+    }
+    $app->call_return( saved => 1 );
+}
+
+sub find_duplicated_assets {
+    my ($app) = @_;
+
+}
 
 sub doLog {
     my ($msg) = @_; 

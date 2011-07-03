a9ad05197be9e1b01703b712b6d15f0ef751d6bf
naoaki011 <naoaki@orion.ocn.ne.jp>
2011-07-03 11:52:25 +0900

    add import menu for asset see #4

--- plugins/AssetHandler/lib/AssetHandler/Util.pm
+++ plugins/AssetHandler/lib/AssetHandler/Util.pm
@@ -10,5 +10,27 @@ sub is_blog_context {
         return 1;
     }
 }
+sub is_mt5 {
+    my $version = MT->version_id;
+    if (($version < 5.1)&&($version >= 5)) {
+        return 1;
+    }
+}
+sub is_illiad {
+    my $version = MT->version_id;
+    if ($version >= 5.1) {
+        return 1;
+    }
+}
+
+sub doLog {
+    my ($msg) = @_; 
+    use MT::Log; 
+    my $log = MT::Log->new; 
+    if ( defined( $msg ) ) { 
+        $log->message( $msg ); 
+    }
+    $log->save or die $log->errstr; 
+}
 
 1;
